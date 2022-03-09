111bf80989a28fd283838883cb3859873298e73d
Leo Cucinell <leocucinell@gmail.com>
2022-03-09 12:20:39 -1000

    base styles to quiz page and addition of questions list to quizPage

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,11 +1,66 @@
 import "./QuizPage.css";
 import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+
+import api from "../../api/api";
 
 const QuizPage = () => {
     //NOTE: Page is reliant on the URL Params... not current user
     const { quizId } = useParams()
+    const [quiz, setQuiz] = useState({})
+
+    useEffect(() => {
+        async function LoadQuiz(){
+            const quizData = await api.get(`quiz/${quizId}`);
+            setQuiz(quizData.data);
+        }
+        LoadQuiz();
+    }, [])
+
+    //NOTE: Delete me after testing page
+    console.log(quiz)
+    //-----------------------------------
+
+    const renderQuestionsList = () => {
+        return(
+            Object.keys(quiz).length !== 0 ?
+                Object.keys(quiz.questions).length !== 0 ?
+                quiz.questions.map((question) => {
+                    return(
+                        <div key={question.id} className="QuizPage-question-card">
+                            <div className="QuizPage-question-side">
+                                <span>{question.title}</span>
+                            </div>
+                            <hr />
+                            <div className="QuizPage-answer-side">
+                                <span>{question.answer}</span>
+                            </div>
+                        </div>
+                    )
+                })
+                :
+                <p>Write some questions to get started!</p>
+            :
+            <p>Loading...</p>
+        )
+    }
+
     return(
-        <h1>QuizPage: {quizId}</h1>
+        <div className="QuizPage-container">
+            <span>{quiz.title}</span>
+            <div className="QuizPage-quiz-container">
+                <div className="QuizPage-options-container">
+                    <p>Options container</p>
+                </div>
+                <div className="QuizPage-quiz">
+                    <p>Quiz Container</p>
+                </div>
+            </div>
+            <hr />
+            <div className="QuizPage-quiestions-list">
+                {renderQuestionsList()}
+            </div>
+        </div>
     )
 }
 
