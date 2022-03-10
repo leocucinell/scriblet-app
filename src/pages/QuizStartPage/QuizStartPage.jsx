9516ded1e9c6815740c15a5e5ef17fa6b2ac4c77
Leo Cucinell <leocucinell@gmail.com>
2022-03-10 08:50:15 -1000

    added quiz start page

--- src/pages/QuizStartPage/QuizStartPage.jsx
+++ src/pages/QuizStartPage/QuizStartPage.jsx
@@ -1,16 +1,38 @@
 import "./QuizStartPage.css"
 import { useParams } from "react-router";
+import { useState, useEffect } from "react";
 
 import QuizDisplay from "../../components/QuizDisplay/QuizDisplay";
+import api from "../../api/api";
 
 const QuizStartPage = () => {
 
     const { quizId } = useParams();
+    const [quiz, setQuiz] = useState({});
+
+    useEffect(() => {
+        async function LoadQuiz(){
+            const quizData = await api.get(`quiz/${quizId}`);
+            setQuiz(quizData.data);
+        }
+        LoadQuiz();
+    }, []);
+
+    const renderQuizCard = () => {
+        return(
+            Object.keys(quiz).length !== 0 ?
+                Object.keys(quiz.questions).length !== 0 ?
+                    <QuizDisplay questions={quiz.questions} />
+                :
+                <p>Write some questions first!</p>
+            :
+            <p>Loading...</p>
+        )
+    }
 
     return(
         <div className="QuizStartPage-container">
-            {/* <QuizDisplay /> */}
-            <p>Quiz Display, after retrieval of data</p>
+            {renderQuizCard()}
         </div>
     )
 }
