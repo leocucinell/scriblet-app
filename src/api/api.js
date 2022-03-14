8771d6ca6b423e084dcd62bbc31b6acc9fb914e9
Leo Cucinell <leocucinell@gmail.com>
2022-03-14 12:41:30 -1000

    test commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/" //NOTE: Edit me when upload api
+    baseURL: "http://18.236.226.185:4500/" //NOTE: Edit me when upload api
 });
 
 export default api
